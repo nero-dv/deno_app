76dc15547fea36fd93961f2a51529560c015df3e
nero-dv <louis_noir@protonmail.com>
2023-01-08 15:20:46 -0600

    remove ConnInfo var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { ConnInfo, serve } from "https://deno.land/std@0.171.0/http/server.ts";
 
 const body: Uint8Array = await Deno.readFile("./index.html");
 
-serve((_req: Request, _connInfo: ConnInfo): Response => {
+serve((_req: Request): Response => {
   return new Response(body);
 }, {
   onListen({ hostname, port }: { hostname: string; port: number }): void {
